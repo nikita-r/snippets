e1a9ad1ed725a0772b39aa6c66bc7340452e33a6
nikita-r <Nikita.Retyunskiy@moodys.com>
2019-09-18 12:57:33 -0400

    init Python; pyodbc

---
+++ Python/hlp.cmd
@@ -0,0 +1,4 @@
+rem Python\hlp.cmd
+
+pip command --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org
+
---
+++ Python/pyodbc.repl.py
@@ -0,0 +1,27 @@
+
+import pyodbc
+
+odbc_cnxn_fstr = 'DRIVER={Microsoft Access Driver (*.mdb)};DBQ=%s;READONLY=True'
+cnxn = pyodbc.connect(odbc_cnxn_fstr % mdb_filepath)
+#cnxn.setdecoding(pyodbc.SQL_WCHAR, 'cp1252')
+cursor = cnxn.cursor()
+for t in cursor.tables(): print(t.table_name)
+...
+for c in cursor.columns(table=''):
+    print(f'[{c.ordinal_position:02d}] {c.column_name}: {c.type_name}')
+...
+for t in tuple(cursor.tables()):
+    if t.table_name.startswith('MSys'): continue
+    _ = cursor.execute('SELECT count(*) FROM [%s]' % t.table_name)
+    print(f'{t.table_name} | rowcount={cursor.fetchone()[0]}')
+...
+sql = 'SELECT %s FROM %s ORDER BY %s' % ()
+try:
+    cursor.execute(sql)
+except pyodbc.ProgrammingError:
+    print('Failed Query: %s' % sql, file=sys.stderr)
+    raise
+while (row := cursor.fetchone()):
+    ...
+
+
