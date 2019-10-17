7dca69f8b5da05ff8b7585a0b80ba7702558db45
nikita-r <nikita-r@asus>
2019-10-17 11:18:01 -0400

    Create New-PSCustomObject.ps1

---
+++ PowerShell/New-PSCustomObject.ps1
@@ -0,0 +1,28 @@
+
+# > New-PSCustomObject @{f1=' a';f2=' b';f3=' c';meta='three little fields'} -DefProps f1,f2,f3
+
+function New-PSCustomObject {
+	[CmdletBinding()]
+
+	param (
+
+	[Parameter(Mandatory,Position=0)]
+	[ValidateNotNullOrEmpty()]
+	[System.Collections.Hashtable]$Hsh,
+
+	[Parameter(Position=1)]
+	[ValidateNotNullOrEmpty()]
+	[Alias('def')]
+	[System.String[]]$DefProps
+	)
+
+	$result = [PSCustomObject]$Hsh
+
+	$ddps = New-Object System.Management.Automation.PSPropertySet `
+			DefaultDisplayPropertySet, $DefProps
+
+	$result | Add-Member -MemberType MemberSet -Name PSStandardMembers `
+			-Value ([Management.Automation.PSMemberInfo[]]$ddps) `
+			-PassThru
+}
+
