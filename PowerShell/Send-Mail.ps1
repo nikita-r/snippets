91bc2d74840ebd23691dda8d6683b3ffcd354b3d
nikita-r <Nikita.Retyunskiy@moodys.com>
2019-11-20 12:13:00 -0500

    Send-Mail+$

--- PowerShell/Send-Mail.ps1
+++ PowerShell/Send-Mail.ps1
@@ -7,7 +7,7 @@ param ( $subj = $(throw), $body = $(throw)
 $addrFrom = "$env:UserName.$([net.dns]::GetHostName())@$env:UserDnsDomain"
 
 $mail = New-Object net.mail.MailMessage($addrFrom, $addr, $subj, $body)
-$attachments |% { $mail.Attachments.Add(New-Object net.mail.Attachment($_)) }
+$attachments |% { $mail.Attachments.Add($(New-Object net.mail.Attachment($_))) }
 
 $smtp = New-Object net.mail.SmtpClient('smtp.sendgrid.net', 587)
 $smtp.EnableSsl = $true
