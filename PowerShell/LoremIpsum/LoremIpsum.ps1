afee09f797b3d4d49df5cb49494fbda1d8b10e42
nikita-r <Nikita.Retyunskiy@moodys.com>
2019-12-04 14:35:00 -0500

    WPF: emulate RecognizesAccessKey="False" in DataGrid

--- PowerShell/LoremIpsum/LoremIpsum.ps1
+++ PowerShell/LoremIpsum/LoremIpsum.ps1
@@ -7,5 +7,10 @@ $App = [Windows.Application]::new()
 
 $MainForm = [windows.markup.XamlReader]::Load([xml.XmlNodeReader] $xaml)
 
+$MainForm.FindName('DataGrid').add_AutoGeneratingColumn({
+    param ($o, [Windows.Controls.DataGridAutoGeneratingColumnEventArgs]$e)
+    $e.Column.Header = $e.Column.Header -replace '_', '__' # emulate RecognizesAccessKey="False"
+})
+
 $ExitCode = $App.Run($MainForm)
 Write-Host ('App.ExitCode='+$ExitCode)
