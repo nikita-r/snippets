d98870b19b2289f52860115d071a172102234fa2
nikita-r <Nikita.Retyunskiy@moodys.com>
2020-01-14 10:48:30 -0500

    Py.const.Err

--- Python/const/const.py
+++ Python/const/const.py
@@ -3,11 +3,13 @@
 class _const:
     class Err(TypeError): pass
     def __setattr__(self, name, value):
-      # if self.__dict__.has_key(name):
-        if name in self.__dict__:
-            raise self.Err('Cannot rebind const.%s' % name)
-        self.__dict__[name]=value
-   #def __delattr__ ?
+      if name in self.__dict__:
+         raise type(self).Err('Cannot rebind const.%s' % name)
+      self.__dict__[name]=value
+    def __delattr__(self, name):
+         raise type(self).Err('Cannot delete const.%s' % name)
+    def __init__(self):
+      self.Err = ... # need to hide the class attribute
 
 import sys
 sys.modules[__name__]=_const()
--- Python/const/main.py
+++ Python/const/main.py
@@ -3,5 +3,8 @@ import const
 
 const.num = 42
 #del const.num
+
+const.Err = ...
+
 const.num = 42
 
