b97f8d3aee0dd47c861b6133852c2389c9854b6c
nikita-r <nikita-r@asus>
2021-05-18 19:45:00 -0400

    OCD re-arrangements

--- tidbits/sil_points.c
+++ tidbits/sil_points.c
@@ -3,18 +3,16 @@ int get_points(int stat)
 {
     int points = 20;
 
-    if (stat >= 0) {
-        for (int i = 0; i < stat; i++) {
-            points += (points + 2) / 5;
-        }
-    } else {
-        int k = 4;
-        for (int i = 0; i < -(stat) && k; i++) {
+    if (stat < 0) {
+        for (int i = stat, k = 4; i && k; ++i, --k) {
             points -= k;
-            --k;
         }
+        return points;
     }
 
+    for (int i = 0; i < stat; ++i) {
+        points += (points + 2) / 5;
+    }
     return points;
 }
 
