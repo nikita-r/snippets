5cfe3dea3d05d393976877fee594c85f9ca2fe41
nikita-r <Nikita.Retyunskiy@moodys.com>
2021-06-21 12:45:00 -0400

    T-SQL: create-proc

---
+++ T-SQL/create-proc.sql
@@ -0,0 +1,42 @@
+/* create-proc.sql */
+
+-- EXEC @rc = [dbo].[sp_...] 1, @rez = @result OUTPUT;
+
+CREATE PROCEDURE [dbo].[sp_...]
+	@flag bit = 'False',
+	@rez int OUTPUT
+AS
+BEGIN
+  SET XACT_ABORT, NOCOUNT ON;
+  BEGIN TRY
+    BEGIN TRAN;
+
+    DECLARE
+      @var nchar(3) = '···',
+      @cnt int = 0;
+
+    IF NOT @flag = 'True'
+    BEGIN
+      ;THROW 5####, 'Invalid @flag', 1;
+    END -- ELSE IF
+
+    SELECT @var = 'abc', @cnt = 3;
+
+    DECLARE @Inserted TABLE (ID int NOT NULL);
+
+    INSERT INTO ...
+    OUTPUT INSERTED.ID INTO @Inserted (ID)
+    VALUES (), (), ...;
+
+    SET @rez = @@ROWCOUNT;
+
+    SELECT @var;
+
+    COMMIT TRAN; RETURN 0;
+  END TRY
+  BEGIN CATCH
+    IF @@TRANCOUNT > 0 ROLLBACK TRAN;
+    THROW
+  END CATCH
+END
+;
