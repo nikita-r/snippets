89bc02d7cbff6c122e5e21eaa7642d28e59dc00f
nikita-r <Nikita.Retyunskiy@moodys.com>
2024-02-15 10:55:00 -0500

    Update pyodbc.repl.py

--- Python/pyodbc.repl.py
+++ Python/pyodbc.repl.py
@@ -1,6 +1,7 @@
 
 import pyodbc
 
+# Microsoft Access Database
 odbc_cnxn_fstr = 'DRIVER={Microsoft Access Driver (*.mdb)};DBQ=%s;READONLY=True'
 cnxn = pyodbc.connect(odbc_cnxn_fstr % mdb_filepath)
 #cnxn.setdecoding(pyodbc.SQL_WCHAR, 'cp1252')
@@ -25,5 +26,30 @@ except pyodbc.ProgrammingError:
     raise
 while (row := cursor.fetchone()):
     ...
+
+cursor.description # schema of a row
+
+# SQL Server
+#;UID=username;PWD=password
+cnxn = pyodbc.connect(driver='{SQL Server}', Trusted_Connection='Yes', server='', database='', readonly=True)
+cursor = cnxn.cursor()
+
+for t in cursor.tables():
+    if t.table_schem == 'dbo' and t.table_type == 'TABLE':
+        print(t.table_name)
+
+for t in cursor.tables():
+    if t.table_schem == 'dbo' and t.table_type == 'VIEW':
+        print(t.table_name)
+
+# Locate non-ASCII in NText
+sql = 'SELECT %s FROM %s WHERE 1=1 AND ( %s ) ORDER BY %s' % (
+    'ArticleId',
+    'Article',
+    'PlainText collate Latin1_General_BIN != cast(PlainText as varchar(max))',
+    'ArticleId deSC')
+cursor.execute(sql)
+while (row := cursor.fetchone()):
+    print(row.ArticleId)
 
 
