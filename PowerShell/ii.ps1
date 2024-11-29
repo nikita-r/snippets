30851636012887aad0b81455b149b11f62ab6e49
nikita-r <Nikita.Retyunskiy@moodys.com>
2024-11-29 13:03:00 -0500

    ii.ps1: Invoke-Sqlcmd, Invoke-Command -AsJob

--- PowerShell/ii.ps1
+++ PowerShell/ii.ps1
@@ -37,6 +37,8 @@ Test-NetConnection sql.url -Port 1433
 
 [AppDomain]::CurrentDomain.GetAssemblies()
 
+<# Prompting #>
+
 $cred = [pscredential]::new($username, (Read-Host password -AsSecureString))
 New-PSSession -Credential $cred | Enter-PSSession
 
@@ -45,15 +47,56 @@ if ($choice -ne 0) { throw }
 
 <# Web #>
 
-function Get-RedirectedUrl ($url) {
+function Get-RedirectionUrl ($url) {
     $request = [net.WebRequest]::Create($url)
     $request.AllowAutoRedirect = $false
     $response = $request.GetResponse()
 
-    if ($response.StatusCode -in 'Found', 'MovedPermanently' ) {
+    if (300 -le $response.StatusCode -and $response.StatusCode -lt 400) {
         $response.GetResponseHeader('Location')
     } else {
         '(' + $response.StatusCode + ')'
     }
 }
+
+<# Invoke-Sqlcmd #>
+
+Import-Module SqlServer -Version 21.1.18256
+$rows = @(Invoke-Sqlcmd <#..#> -ServerInstance SI -Database DB -Query @"
+"@)
+Write-Host $rows.Count
+$rows | Out-GridView -Title <#..#> -PassThru | Format-Table
+
+
+<# Parallel Jobs #>
+<# Remotely Echo #>
+
+$sb = { c:\scripting\try.ps1 }
+$machines = Get-ADComputer -Filter * |% DNSHostName
+$jobs = $machines |% { Invoke-Command -AsJob -Credential $cred -ComputerName $_ -ScriptBlock $sb }
+
+Get-EventSubscriber -SourceIdentifier DataAdded-* | Unregister-Event -ea:0
+$jobsCount = 0
+$jobs |% { Register-ObjectEvent -InputObject $_.ChildJobs[0].Output -EventName DataAdded `
+                                                            -SourceIdentifier "DataAdded-$((++$jobsCount).ToString().PadLeft(3, '0'))" -MessageData $jobsCount }
+$jobs |% { Register-ObjectEvent -InputObject $_ -EventName StateChanged }
+
+while ($jobs |? State -eq 'Running') {
+    Wait-Event |? SourceIdentifier -like 'DataAdded-*' `
+    |% { $output = Receive-Job $jobs[$_.MessageData - 1]
+        Write-Host @($output).Count; $_ } | Remove-Event
+}
+
+$jobs | Receive-Job
+
+$jobs |? State -eq 'Failed' |% {
+    Write-Host 'Failed:' $_.ChildJobs[0].Location $_.ChildJobs[0].Command
+    Write-Host 'Reason:' $_.ChildJobs[0].JobStateInfo.Reason.GetType()
+    Write-Host $_.ChildJobs[0].JobStateInfo.Reason.Message
+    Write-Host
+}
+
+$jobs | Remove-Job -Force
+Get-Event | Remove-Event
+
 
--- tidbits/sil_points.c
+++ tidbits/sil_points.c
@@ -1,20 +1,20 @@
 
 int get_points(int stat)
 {
-    int points = 20, i = stat;
+	int points = 20, i = stat;
 
-    if (stat < 0) for (int k = 4; i && k; ++i, --k) points -= k;
-    else for (; i; --i) points += (points + 2) / 5;
+	if (stat < 0) for (int k = 4; i && k; ++i, --k) points -= k;
+	else for (; i; --i) points += (points + 2) / 5;
 
-    return points;
+	return points;
 }
 
 #include <stdio.h>
 
 int main()
 {
-    for (int i = -5; i < 8; ++i) {
-        printf("%+i: %i\n", i, get_points(i));
-    }
-    return 0;
+	for (int i = -5; i < 8; ++i) {
+		printf("%+i: %i\n", i, get_points(i));
+	}
+	return 0;
 }
