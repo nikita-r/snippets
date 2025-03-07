fb17d2ef30a215e82f2b9b15ae52f37beff5b724
nikita-r <Nikita.Retyunskiy@moodys.com>
2025-03-07 11:30:00 -0500

    Update re_json.py, tablesJoiner.PQM

--- Python/re_json.py
+++ Python/re_json.py
@@ -4,18 +4,27 @@ import json
 
 data = []
 ...
-with open('./wrangle/datas.json', 'a', encoding='utf-8') as fp:
+with open('./wrangle/datas.json', 'x', encoding='utf-8', newline='\n') as fp:
     json.dump(data, fp, indent='\t')
 
 data = []
 ...
-with open('./wrangle/datas.json', 'a', encoding='utf-8') as fp:
+with open('./wrangle/datas.json', 'a', encoding='utf-8', newline='\n') as fp:
     json.dump(data, fp, indent='\t')
 
 fp = open('./wrangle/datas.json', 'r+b')
-offset = fp.read().find(b'\n][')
-assert offset > -1
-fp.seek(offset + 1)
-fp.write(b'\t,')
-fp.close()
+offset = fp.read().find(b'\n][\n')
+try:
+  if offset > -1:
+    fp.seek(offset + 1)
+    fp.write(b'\t,')
+  else:
+    fp.seek(0)
+    offset = fp.read().find(b'\n][]')
+    assert offset > -1
+    fp.seek(offset + 2)
+    fp.truncate()
+finally:
+    fp.close()
+
 
